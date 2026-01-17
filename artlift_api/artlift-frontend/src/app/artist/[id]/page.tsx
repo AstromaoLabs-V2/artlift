4b5be4d08f37d8ea622570e67c1f41759f7fb9ed
岩江蒼樹 <sokiiwae@MacBook-Pro-56.local>
2026-01-17 13:12:16 +0900

    Artis detail structure change and align to main

--- artlift_api/artlift-frontend/src/app/artist/[id]/page.tsx
+++ artlift_api/artlift-frontend/src/app/artist/[id]/page.tsx
@@ -2,16 +2,25 @@
 
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import {getArtist} from "@/app/lib/Artists/[id]/getArtists";
+import Navigation from "@/app/components/nav/navigation";
+import FollowSubscribe from "@/app/components/features/FollowSubscribe";
+
 
 interface Artist {
   first_name: string;
   last_name: string;
+  bg: string;
+  img:string;
+  about:string;
+
 }
 
 export default function ArtistPage({
   params,
 }: {
   params: Promise<{ id: string }>;
+
 }) {
   const { id } = React.use(params);
   const [artist, setArtist] = useState<Artist | null>(null);
@@ -24,38 +33,38 @@ export default function ArtistPage({
       router.push("/login");
       return; 
     }
-    const fetchArtist = async () => {
-      try {
-        const res = await fetch(`http://localhost:8000/artist/${id}/`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        });
-        if (res.status === 401) {
-          router.push("/login");
-          return;
-        }
-        if (!res.ok) {
-          console.error("Failed to fetch artist", res.status);
-          return;
-        }
-        const data = await res.json();
-        setArtist(data);
-      } catch (err) {
-        console.error(err);
-        router.push("/login");
-      }
-    };
 
-    fetchArtist();
+    getArtist(id,token).then((data)=>{
+      setArtist(data);
+    })
+    .catch((err)=>{
+      if (err.message === "UNAUTHORIZED") {
+        router.push("/login");
+      } else {
+        console.error(err);
+      }
+    });
   }, [id, router]);
 
   if (!artist) return <p>Loading artist...</p>;
 
   return (
-    <p className="text-2xl text-amber-200">
-      {artist.first_name} {artist.last_name}
-    </p>
+    <>
+    <Navigation />
+     <img className="w-full"src={artist.bg} />
+    <FollowSubscribe
+  artistIcon={artist.img}
+  artistName={artist.first_name}/>
+
+  <h2>Popular Artworks</h2>
+  {/*This component is coming from artrok section. It is related to users ID*/}
+
+  <h2>About</h2>
+  <p>{artist.about}</p>
+
+
+    </>
+   
+
   );
 }
